77da1831b5e5d8d32ad17a6ef04bcede865191c4
Dexter Small <dexterarron@gmail.com>
2020-10-09 16:05:46 -0400

    Added code according to ex2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,12 @@
 "use strict"
 
+let current_player = "X"
+let game_squares = 0
+let game_state = []
+
 window.onload = function() {
 	var board = document.getElementById("board");
+
 	setupBoard(board)
 }
 
@@ -11,5 +16,34 @@ function setupBoard(board) {
 
 	for (var i = 0; i < board_children_amount; i++) {
 		board.getElementsByTagName('div')[i].classList.add('square');
+		board.getElementsByTagName('div')[i].addEventListener('click', handleSquareClick);
 	}
 }
+
+// Handle square click
+function handleSquareClick(click_event) {
+	var clicked_square = click_event.target
+
+	if (current_player == "X") { 
+		clicked_square.classList.add(current_player)
+		clicked_square.innerHTML = current_player
+		current_player = "0"
+		game_squares = game_squares + 1
+		updateGameState()
+	} else {
+		clicked_square.classList.add(current_player)
+		clicked_square.innerHTML = current_player
+		current_player = "X"
+		game_squares = game_squares + 1
+		updateGameState()
+	}
+
+
+}
+
+// Update Game State
+function updateGameState() {
+	game_state.push(current_player)
+}
+
+
